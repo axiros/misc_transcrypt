6b00f7aee681fabf302b5c5c74356494e7dbee08
Gunther Klessinger <gunther.klessinger@axiros.com>
2016-07-26 14:47:52 +0200

    deco

--- doc/kendo/src/ch4/pykendo.py
+++ doc/kendo/src/ch4/pykendo.py
@@ -1,3 +1,45 @@
+def debug(obj, func, *a):
+    console.log('before call', a)
+    res = func(obj, *a)
+    console.log('after call, got', res)
+    return res
+
+
+def decorate(cls, func, dfunc):
+    """
+    class : a Transcrypt class
+    func  : name of method to decorate
+    dfunc : decorator function
+
+    Example:
+        e.g. def mydeco(obj, func, *a): return func(obj, *a)
+    """
+    def d3(*a):
+        # stage 3: call the decorator like known in python (obj, func, args):
+        return this['dfunc'](this['self'], this['orig'], *a)
+    def d2(f, dfunc):
+        # stage2: return stage3 function, with context
+        return lambda: d3.bind({'self': this, 'orig': f, 'dfunc': dfunc})
+    # stage1: define the getter, func = name of original function:
+    cls.__defineGetter__(func, d2(cls[func], dfunc))
+
+class A:
+    i = 2
+    def foo(self, bar, baz):
+        return int(bar) * int(baz) * self.i
+
+console.log(A().foo(1, 2))
+
+decorate(A, 'foo', debug)
+
+a = A()
+console.log(a.foo(3, 2))
+
+
+debugger
+
+
+
 import datasource, datepicker, dropdownlist, grid
 import themeroller
 # TS does currently not expose our imports, so we redeclare, explicitly:
--- doc/kendo/src/ch4/tools.py
+++ doc/kendo/src/ch4/tools.py
@@ -1,5 +1,34 @@
 import time
 
+
+def decorate(cls, func, dfunc):
+    """
+    class : a Transcrypt class
+    func  : name of method to decorate
+    dfunc : decorator function
+
+    Example:
+        e.g. dfunc =
+            def mydeco(obj, func, *a): return func(obj, *a)
+        class A:
+            i = 2
+            def foo(self, j, k): return self.i * int(j) * int(k)
+
+        decorate(A, 'foo', dfunc)
+        A().foo(4, 5) -> will pass the args and the result through the mydeco
+
+    """
+    def d3(*a):
+        # stage 3: call the decorator like known in python (obj, func, args):
+        return this['dfunc'](this['self'], this['orig'], *a)
+    def d2(f, dfunc):
+        # stage2: return stage3 function, with context
+        return lambda: d3.bind({'self': this, 'orig': f, 'dfunc': dfunc})
+    # stage1: define the getter, func = name of original function:
+    cls.__defineGetter__(func, d2(cls[func], dfunc))
+
+
+
 def name_value_pairs(l):
     ret = []
     for k, v in l:
